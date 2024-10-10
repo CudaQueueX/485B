7ac38de1833dad6929e1eb710189f010c05052d4
Mathew Terhune <mathewterhune@gmail.com>
2024-10-10 14:30:17 -0700

    Update: Simple Implementation of MinHeap

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 // main.cpp
 
+/**
+ * Reference implementation of a MinHeap that is only run on the CPU.
+ */
+
+
 #include <iostream>
+#include <vector>
+
+#include "include/CPP/MinHeap.h"
+#include "include/CPP/BPQ.h";
 
 int main() {
     std::cout << "Mathew's Branch" << std::endl;
     std::cout << "Hello world" << std:: endl;
+
+    // initalize the heap
+    MinHeap<int> minHeap(6);
+    std::vector<int> arr = {15,10,5,4,3,2};
+    
+    // Build the minheap
+    std::cout << "Building Heap" << std::endl;
+    minHeap.buildHeap(arr);
+    std::cout << "Heap Built" << std::endl;
+
+    std::cout << "Heap Contents" << std::endl;
+    minHeap.printHeap();
+
+    
+
+
+
+
+
     return 0;
 }
